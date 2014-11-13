489aa44997260a844b8ef8d699c0b0eeecddef24
John Rotach <john@2lemetry.com>
2014-11-12 22:42:57 -0700

    Initial code commit

---
+++ src/main/scala/org/rotach/Twos.scala
@@ -0,0 +1,19 @@
+package org.rotach
+
+class Twos {
+  def twos(nums: Array[Int]): Int = {
+    if (nums.size == 1) {
+      return if (nums(0) == 2) 1 else 0
+    } else {
+      return (if (nums(0) == 2) 1 else 0) + twos(nums.slice(1,nums.size))
+    }
+  }
+
+  def nottwos(nums: Array[Int]): Int = {
+    if (nums.size == 1) {
+      return if (nums(0) == 2) 0 else 1
+    } else {
+      return (if (nums(0) == 2) 0 else 1) + nottwos(nums.slice(1,nums.size))
+    }
+  }
+}
---
+++ src/test/scala/TwoTest.scala
@@ -0,0 +1,30 @@
+import org.rotach.Twos
+import org.scalatest._
+
+class TwoTest extends FlatSpec with Matchers {
+
+  "twos" should "return 0 if there are no twos" in {
+    val t = new Twos()
+    t.twos(Array(1, 4, 5, 6, 3)) should be (0)
+  }
+
+  "twos" should "return 1 if there is one two" in {
+    val t = new Twos()
+    t.twos(Array(1, 4, 5, 2, 6, 3)) should be (1)
+  }
+
+  "twos" should "return 2 if there are two twos" in {
+    val t = new Twos()
+    t.twos(Array(2, 1, 4, 5, 6, 3, 2)) should be (2)
+  }
+
+  "nottwos" should "return 5 if there are no twos in a 5 element array" in {
+    val t = new Twos()
+    t.nottwos(Array(1, 4, 5, 6, 3)) should be (5)
+  }
+
+  "nottwos" should "return 1 if there are no twos in a 1 element array" in {
+    val t = new Twos()
+    t.nottwos(Array(4)) should be (1)
+  }
+}
